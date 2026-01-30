69aa5ba8b89bed38a3618dd973218c6fbdb2fd23
Haldi247 <magicalhadeeqa@gmail.com>
2026-01-30 12:54:12 +0500

    Clean up commented-out code in constraint_cl.py
    Removed commented-out methods for adding constraints and checking validity in constraint_cl.py.

--- constraint_cl.py
+++ constraint_cl.py
@@ -7,18 +7,6 @@ class CSP:
         for variable in variables:
             self.constraints[variable] = []
 
-    # def add_constraint(self, var1, var2, constraint_func):  #Add a binary constraint between var1 and var2
-        
-    #     self.constraints[var1].append((var2, constraint_func(var1,var2)))
-    #     self.constraints[var2].append((var1, constraint_func(var2, var1)))
-
-    # def is_valid(self, assignment, var, value): #Check if assigning value to var is valid with current assignment
-        
-    #     for (neighbor, constraint) in self.constraints[var]:
-    #         if neighbor in assignment and not constraint:
-    #             return False
-    #     return True
-
     def add_constraint(self, var1, var2, constraint_func):
         # Add a binary constraint between var1 and var2
         self.constraints[var1].append((var2, constraint_func))
@@ -41,8 +29,6 @@ class CSP:
         unassigned = [v for v in self.variables if v not in assignment]
         first = unassigned[0]
         for value in self.domains[first]:
-            # print ("backtrack classes: ",  value[0])
-            # print(value[1])
             if self.is_valid(assignment, first, value):
                 local_assignment = assignment.copy()
                 local_assignment[first] = value
